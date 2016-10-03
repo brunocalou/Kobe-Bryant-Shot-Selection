aa9da9e80ecce6db93de2edcdbcd92147fe8da51
Heitor Guimaraes <Heitor Guimaraes>
2016-10-03 17:17:06 -0300

    Idea for pre-processing phase

---
+++ R/utils/devdependencies.R
@@ -0,0 +1,9 @@
+#  Name: install_dependencies 
+#  Objective: Utility to help install some packages/libraries from
+# Cran or Github necessary for this project
+
+install_dependencies <- function(){
+  print("Installing Project dependencies/libraries... Please wait")
+  # Insert dependencies here
+  print("Status: Success")
+}
