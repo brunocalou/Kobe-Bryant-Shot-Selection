a20fa3390dd9243128900222527e77ba570f9df0
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-12-06 12:54:02 -0200

    Normalize the data and start the model knn

--- R/utils/devdependencies.R
+++ R/utils/devdependencies.R
@@ -5,19 +5,9 @@
 install_dependencies <- function(){
   print("Installing Project dependencies/libraries... Please wait")
   # Insert dependencies here
-  list.of.packages <- c("GGally", "corrplot")
+  list.of.packages <- c("GGally", "corrplot", "caret", "ISLR", "nnet")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org", dependencies = TRUE)
-  
-  # Helpers
-  install.packages("GGally", repos = "http://cran.us.r-project.org")
-  install.packages("corrplot", repos = "http://cran.us.r-project.org")
-  install.packages("nloptr", repos = "http://cran.us.r-project.org")
-  install.packages("caret", repos = "http://cran.us.r-project.org")
-  install.packages("ISLR", repos = "http://cran.us.r-project.org")
-  
-  # Models
-  install.packages("nnet", repos = "http://cran.us.r-project.org")
   
   print("Status: Success")
 }
